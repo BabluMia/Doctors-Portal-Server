cad1f6efc44938c24d9e0eb90897ceaf1dddb583
Bablu Mia <bablumiah2000@gmail.com>
2022-05-16 20:10:01 +0600

    admin added

--- index.js
+++ index.js
@@ -61,24 +61,39 @@ async function run() {
       res.send(services);
     });
     // all user
-    app.get('/user',verifyJWT, async(req,res)=>{
-      const users = await userCollection.find().toArray()
-      res.send(users)
-
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    // admin 
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
     })
     // admin role
-    app.put("/user/admin/:email", async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: {role:'admin'},
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send(result);
+      }
+      else{
+        res.status(403).send({ message: "Forbidden access" });
+      }
     });
     // user collection
-    app.put("/user/:email",verifyJWT, async (req, res) => {
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -90,7 +105,7 @@ async function run() {
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "8h" }
+        { expiresIn: "24h" }
       );
       res.send({ result, token });
     });
@@ -131,11 +146,9 @@ async function run() {
         const query = { patient: patient };
         const bookings = await bookingCollection.find(query).toArray();
         res.send(bookings);
-      }
-      else{
+      } else {
         return res.status(403).send({ message: "Forbidden access" });
       }
-      
     });
 
     // post booking
